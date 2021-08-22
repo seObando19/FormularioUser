d9aa04cb31f5d5bec5740c855e7ee01ef59b9219
Sebastian Obando <sebastiano-10@hotmail.com>
2021-08-22 18:48:18 -0500

    Update to database config

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config({path:'./.env'});
 
-//pass: CUQaL7NtfVHmaHeY user: admin
-
-mongoose.connect('mongodb://localhost/mean-users',{useNewUrlParser: true, useUnifiedTopology: true})
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const name = process.env.DB_NAME;
+mongoose.connect(`mongodb+srv://${user}:${pass}@server1.h8sfo.mongodb.net/${name}?retryWrites=true&w=majority`,
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+    }
+)
     .then((db) => console.log('Database Connected'))
     .catch((err) => console.log(err))
